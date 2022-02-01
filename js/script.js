56739f5a8999d875d3bd40698e4382c1cfc536ed
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-02-01 18:12:19 +0100

    JS added

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('#list');
+let list = $('#list');
+let inputValue = $('#input').val();
+$('.selectedElement').append('<li>some item</li>')
+li.append(inputValue);
+li.addClass("strike");
+li.on("click", function() {
+    li.addClass("strike");
+  });
+deleteButton.append(document.createTextNode('X'));
